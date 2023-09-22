6b4abde4873a55e8c5356566a2a09727ab78ed04
hyewonkim <hwkim@tidesquare.com>
2023-09-22 14:25:13 +0900

    feat: Image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.realworld.io',
+        port: '',
+        pathname: '/images/*' /* /images/**는 숫자만 먹음 */,
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
